c7f2a5c08ce153551f942581704cc18732576985
eliaschank10 <calivaelias621@gmail.com>
2025-03-18 14:50:43 -0300

    primeros pasos

---
+++ Elias/src/primerospasos/hola_mundo.java
@@ -0,0 +1,14 @@
+package primerospasos;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+public class hola_mundo {
+    
+    public static void main (String[] args) {
+        
+        Scanner guardar = new Scanner (System.in);
+        System.out.print("INGRESAR TU EDAD: ");
+        int edad=guardar.nextInt();
+        System.out- println("tu edad es: "+edad);
+    }
+    
+}
